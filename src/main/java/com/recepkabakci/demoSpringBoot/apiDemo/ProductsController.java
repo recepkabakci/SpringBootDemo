b9e0a9391001f4147ac603866373570fb393dde1
recepkabakci <103920618+recepkabakci@users.noreply.github.com>
2023-03-08 11:05:06 +0100

    update

--- src/main/java/com/recepkabakci/demoSpringBoot/apiDemo/ProductsController.java
+++ src/main/java/com/recepkabakci/demoSpringBoot/apiDemo/ProductsController.java
@@ -1,4 +1,4 @@
-package com.javacourse.demoSpringBoot.apiDemo;
+package com.recepkabakci.demoSpringBoot.apiDemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/recepkabakci/demoSpringBoot/DemoSpringBootApplicationTests.java
+++ src/test/java/com/recepkabakci/demoSpringBoot/DemoSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javacourse.demoSpringBoot;
+package com.recepkabakci.demoSpringBoot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
